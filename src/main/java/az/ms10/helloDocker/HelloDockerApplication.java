0451df736741663fc959d5758edd0c1992bcb7f9
Elgun Shukurov <elgunshukurov@yahoo.com>
2022-05-30 11:33:04 +0400

    initial commit

---
+++ src/main/java/az/ms10/helloDocker/HelloDockerApplication.java
@@ -0,0 +1,13 @@
+package az.ms10.helloDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/ms10/helloDocker/HelloDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ms10.helloDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
